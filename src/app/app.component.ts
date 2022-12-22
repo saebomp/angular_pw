3e638a3b31d09227df0c34cf5c00b0f66dbb7486
SAEBOMPAK <saebompak@gmail.com>
2022-12-22 15:38:57 -0800

    finish password generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0;
   includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
   password = '';
 
   // constructor() {
   //   this.password = '';
   // }
+  onChangeLength(value:string){
+    const parsedValue = parseInt(value);
+    // parseInt : string to integer
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
 
   onChangeUseNumbers() {
-
+    this.includeNumbers = !this.includeNumbers;
   }
 
   onChangeUseSymbols() {
-    
+    this.includeSymbols = !this.includeSymbols;
   }
 
   onChangeUseLetters () {
@@ -26,9 +37,30 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(this.includeLetters);
-    this.password = 'MY password!!!!!!!!!';
+    const numbers = '123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
+
+  
   getName() {
     return 'dddf'
   }
