33c082b0dabbbf595950cd9d03534eb5f80aa51a
SAEBOMPAK <saebompak@gmail.com>
2022-12-22 13:59:25 -0800

    initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  password = '';
+
+  // constructor() {
+  //   this.password = '';
+  // }
+
+  onChangeUseNumbers() {
+
+  }
+
+  onChangeUseSymbols() {
+    
+  }
+
+  onChangeUseLetters () {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onButtonClick() {
+    console.log(this.includeLetters);
+    this.password = 'MY password!!!!!!!!!';
+  }
+  getName() {
+    return 'dddf'
+  }
 }
